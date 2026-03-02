c0b50a7b490252514566d5df9212c768293ad042
mtlee07 <maxwell.lee.372007@gmail.com>
2026-03-02 17:36:23 -0500

    better deploy seems to work

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # deploys code to defined device through rsync, and then runs the simulation with hwSim flag
 # keep in mind that this requires ssh keys to be set up for passwordless login
-cd /mnt/c/Users/maxwe/diffy-swerve-from-scratch-1
-URL="sparky@10.93.12.2" #todo: fix
+URL_MASTER="sparky@10.93.12.2"
+URL_SLAVE="sparky@10.93.12.5" 
 DESTINATION="/home/sparky/diffy-swerve-from-scratch/"
-echo $pwd
-echo "Deploying to $URL:$DESTINATION"
-rsync -avz --exclude-from='.rsync-exclude' ./ sparky@10.93.12.2:/home/sparky/diffy-swerve-from-scratch/
+echo "$(pwd)"
+echo "Deploying to ${URL_MASTER}:${DESTINATION} and ${URL_SLAVE}:${DESTINATION}"
+rsync -avz --exclude-from='.rsync-exclude' ./ "${URL_MASTER}:${DESTINATION}"
+rsync -avz --exclude-from='.rsync-exclude' ./ "${URL_SLAVE}:${DESTINATION}"
 echo "Deployment complete!"
 
 echo "running remote sim and piping output to terminal..."
-ssh -t "sparky@10.93.12.2" "cd ./diffy-swerve-from-scratch && ./gradlew simulateJava -PhwSim"
+ssh -t "${URL_MASTER}" "cd ./diffy-swerve-from-scratch && ./gradlew simulateJava -PhwSim" &
+ssh -t "${URL_SLAVE}" "cd ./diffy-swerve-from-scratch && ./gradlew simulateJava -PhwSim" &
+wait
