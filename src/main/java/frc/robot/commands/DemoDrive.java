d6bafae22fcf995f8030c7fc4736f91537e3f1e4
mtlee07 <maxwell.lee.372007@gmail.com>
2026-03-03 17:16:08 -0500

    Untested addiiton of logic to swap robot positions after seeing wing

--- src/main/java/frc/robot/commands/DemoDrive.java
+++ src/main/java/frc/robot/commands/DemoDrive.java
@@ -1,12 +1,17 @@
 
 package frc.robot.commands;
 
+import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.networktables.BooleanPublisher;
 import edu.wpi.first.networktables.BooleanSubscriber;
+import edu.wpi.first.networktables.IntegerPublisher;
+import edu.wpi.first.networktables.IntegerSubscriber;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.PubSubOption;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -26,7 +31,8 @@ public class DemoDrive extends SequentialCommandGroup {
     BooleanSubscriber isOtherRobotFinished;
     
 
-    Supplier<Integer> wingApproximateToUse;
+    Supplier<Integer> wingEstimateApproximateToUse;
+    Supplier<Integer> wingVisionApproximateToUse;
 
     /** Creates a new DemoDrive. */
     public DemoDrive(Swerve swerve, Lift lift, WingPoseEstimator wingPoseEstimator, InputGetter inputGetter) {
@@ -35,7 +41,7 @@ public class DemoDrive extends SequentialCommandGroup {
         // have the master determine which wing position is closer
         if(Constants.IS_MASTER) {
             BooleanPublisher useMasterTagPublisher = NetworkTableInstance.getDefault().getTable("DemoMode").getBooleanTopic("use master tag").publish();
-            wingApproximateToUse = () -> {
+            wingEstimateApproximateToUse = () -> {
             if(Math.abs(swerve.getPose().minus(DemoConstants.wingApproximates[0]).getTranslation().getNorm()) < (Math.abs(swerve.getPose().minus(DemoConstants.wingApproximates[1]).getTranslation().getNorm()))) {
                 useMasterTagPublisher.accept(false);
                 return 0;
@@ -45,13 +51,14 @@ public class DemoDrive extends SequentialCommandGroup {
             }};
         } else {
             BooleanSubscriber useMasterTagSubscriber = NetworkTableInstance.getDefault().getTable("DemoMode").getBooleanTopic("use master tag").subscribe(true);
-            wingApproximateToUse = () -> useMasterTagSubscriber.get() ? 0 : 1;
+            wingEstimateApproximateToUse = () -> useMasterTagSubscriber.get() ? 0 : 1;
         }
 
         
         addCommands(
-            new AutoDrive(swerve, () -> DemoConstants.wingApproximates[wingApproximateToUse.get()], true), // follow path to approximate wing position
-            new AutoDrive(swerve, () -> wingPoseEstimator.getEstimatedPose().plus(DemoConstants.wingRelativeFormationOffsets[wingApproximateToUse.get()]), false), // PID to exact wing position
+            new AutoDrive(swerve, () -> DemoConstants.wingApproximates[wingEstimateApproximateToUse.get()], true), // follow path to approximate wing position
+            Constants.IS_MASTER ? new InstantCommand(() -> chooseWingVisionApproximateToUse(swerve, wingPoseEstimator)).andThen(new WaitUntilCommand(isOtherRobotFinished::get)) : new WaitUntilCommand(isOtherRobotFinished::get).andThen(new InstantCommand(()->applyWingApproximate())),
+            new AutoDrive(swerve, () -> wingPoseEstimator.getEstimatedPose().plus(DemoConstants.wingRelativeFormationOffsets[wingVisionApproximateToUse.get()]), false), // PID to exact wing position
             new WaitUntilCommand(isOtherRobotFinished::get), // wait for other robot to finish
             lift.setLiftState(LiftPosition.pickup), // lift up
             new WaitCommand(5), // someone puts the thing on the lift
@@ -60,7 +67,23 @@ public class DemoDrive extends SequentialCommandGroup {
             // new InstantCommand(RobotConfig::resetOffsetPositions), // sets ofsets to original
             // new TandemDrive(swerve, inputGetter::getJoystickVelocity).until(inputGetter::getRightBumper), // tandem drive with other robot manually
             new FollowPath(swerve, new Path(PathConstants.wayPoints, PathConstants.defaultSpeed, PathConstants.lookAhead, PathConstants.rotationalLookAhead), DemoConstants.stationPosition) // follow path to station
-
         );
+    }
+
+    public void chooseWingVisionApproximateToUse(Swerve swerve, WingPoseEstimator wingPoseEstimator) {
+        IntegerPublisher useMasterTagPublisher = NetworkTableInstance.getDefault().getTable("DemoMode").getIntegerTopic("use master tag post-vision").publish();
+            wingVisionApproximateToUse = () -> {
+            if(Math.abs(swerve.getPose().minus(wingPoseEstimator.getEstimatedPose().plus(DemoConstants.wingRelativeFormationOffsets[0])).getTranslation().getNorm()) < Math.abs(swerve.getPose().minus(wingPoseEstimator.getEstimatedPose().plus(DemoConstants.wingRelativeFormationOffsets[1])).getTranslation().getNorm())) {
+                useMasterTagPublisher.accept(1);
+                return 0;
+            } else {
+                useMasterTagPublisher.accept(0);
+                return 1;
+            }};
+    }
+
+    public void applyWingApproximate() {
+            IntegerSubscriber useMasterTagSubscriber = NetworkTableInstance.getDefault().getTable("DemoMode").getIntegerTopic("use master tag post-vision").subscribe(-1);
+            wingVisionApproximateToUse = () -> ((int)useMasterTagSubscriber.get());
     }
 }
